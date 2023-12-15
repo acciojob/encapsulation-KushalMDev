d32ff511d41ad7dee22b24603dd6a3084390ca3e
KushalMDev <kushm1929@gmail.com>
2023-12-15 19:56:52 +0530

    Encapsulation commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,24 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly rwobject = new RWOnly();
+        // obj.name="Hello";
+        //System.out.println(obj.name);
+        // Main.java:5: error: cannot find symbol
+        /*
+         * RWOnly obj=new RWOnly();
+         * ^
+         * symbol: class RWOnly
+         * location: class Main
+         * Main.java:5: error: cannot find symbol
+         * RWOnly obj=new RWOnly();
+         * ^
+         * symbol: class RWOnly
+         * location: class Main
+         * 2 errors
+         */
+        rwobject.setName("Kushal");
+        System.out.println(rwobject.getName());
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -2,4 +2,17 @@ package com.driver;
 
 public class RWOnly {
     
+    private String name;
+
+    public RWOnly() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
